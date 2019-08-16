4b2ea72c81ffeb527b0451ef9316c8cf8f3c39c2
Jennifer Garcia <jen.garcia91@gmail.com>
2019-08-16 12:09:31 -0700

    added home.html and first redirect. We be cool

--- app.py
+++ app.py
@@ -12,8 +12,21 @@ debug = DebugToolbarExtension(app)
 
 
 @app.route('/')
+def landing_page():
+    return render_template("home.html")
+
+
+@app.route("/createsession", methods=["POST"])
+def intialize_data():
+
+    session["board"] = boggle_game.make_board()
+    return redirect("/gamestart")
+
+
+@app.route('/gamestart')
 def start_game():
     """Display game board"""
-    return render_template('/index.html')
+    return render_template('index.html')
+
 
 
