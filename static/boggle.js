4586c9f5094595ec9ce52d569cacdf09e34d92b7
Jennifer Garcia <jen.garcia91@gmail.com>
2019-08-16 15:24:20 -0700

    Added timer and score. Kieran is cool

--- static/boggle.js
+++ static/boggle.js
@@ -1,9 +1,31 @@
 $(function() {
+    var count = 60;
+    setTimeout(function() {
+        $("#submit-guess").off();
+        $("#submit").prop("disabled", true);
+        clearInterval(counter)
+    }, 60000)
+    let counter = setInterval(function(){
+        count -= 1
+        $("#timer").text(count)
+    }, 1000)
+
+
     $('#submit-guess').on('submit', async function(e) {
         e.preventDefault();
         let guess = e.target.children[0].value;
         response = await axios.post('/submitguess', {guess: guess});
         $('#result').text(response.data.result)
-        })
+        updateScore(response.data.result, guess.length);
+        });
+
+   function updateScore(result, guess) {
+        if(result === "ok") {
+            $("#score-num").text(guess);
+        }
+   };     
+
+
+
 })
 
